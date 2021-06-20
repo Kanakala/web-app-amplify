976d3c9ea87e4423a3324505c467ebdd80f0b04e
SUMANTH KANAKALA <sumanth.k@aithinkers.com>
2021-06-20 21:43:11 +0530

    Added Sample AWS Exports file

---
+++ src/sample-aws-exports.js
@@ -0,0 +1,14 @@
+const awsconfig = {
+    aws_app_analytics: 'enable',
+    aws_user_pools: 'enable',
+    aws_user_pools_id: 'us-east-1*****',
+    aws_user_pools_mfa_type: 'OFF',
+    aws_user_pools_web_client_id: 'deev**********',
+    aws_user_settings: 'enable',
+    aws_cognito_region: 'us-east-1',
+    aws_cognito_identity_pool_id: 'us-east-1:91*****-e2fa-4a10-a220-e5d******',
+    aws_appsync_graphqlEndpoint: 'https://swetz***********.appsync-api.us-east-1.amazonaws.com/graphql',
+    aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS"
+};
+
+export default awsconfig
